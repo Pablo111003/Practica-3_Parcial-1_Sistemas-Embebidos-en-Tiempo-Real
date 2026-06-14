95e2ebf7598c09519a93a6f57a5e1753409f2b57
Pablo111003 <9129@soyunaq.mx>
2026-06-14 14:49:14 -0600

    Add files via upload

---
+++ app_config.h
@@ -0,0 +1,18 @@
+#ifndef APP_CONFIG_H
+#define APP_CONFIG_H
+
+#define LED_B0      2
+#define LED_B1      4
+#define LED_B2      5
+#define LED_B3      18
+
+#define BTN_DIR     13
+#define BTN_SPEED   14
+#define BTN_START   15
+
+#define SPEED_SLOW_MS   500
+#define SPEED_FAST_MS   250
+
+#define BUTTON_POLL_MS  20
+
+#endif
