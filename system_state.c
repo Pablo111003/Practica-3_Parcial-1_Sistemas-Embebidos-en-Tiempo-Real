a87a896f7e73fd8b6f6ddced13f48f0d9b748c33
Pablo111003 <9129@soyunaq.mx>
2026-06-20 22:14:06 -0600

    Update system_state.c

--- system_state.c
+++ system_state.c
@@ -1,25 +1,23 @@
 #include "system_state.h"
 #include "app_config.h"
 
-/* Variable global que contiene el estado completo del sistema.
-   Todas las tareas la leen/escriben para coordinar el comportamiento. */
+// Variable global que contiene el estado completo del sistema. Todas las tareas la leen/escriben para coordinar el comportamiento.
 SystemState_t g_system;
 
 void system_state_init(void)
 {
-    /* 1. El contador arranca en cero (0000 en binario = ningún LED encendido) */
+    // 1. El contador arranca en cero
     g_system.value = 0;
 
-    /* 2. Velocidad inicial: lento (500 ms por paso) */
+    // Velocidad inicial: lento (500 ms)
     g_system.period_ms = SPEED_SLOW_MS;
 
-    /* 3. Dirección inicial: ascendente (0 → 9) */
+    // Dirección inicial: ascendente (0 → 9)
     g_system.direction = COUNT_UP;
 
-    /* 4. Modo inicial: pausado.
-       El sistema NO cuenta hasta que se presione START por primera vez. */
+    // Modo inicial: pausado. El sistema NO cuenta hasta que se presione START por primera vez
     g_system.mode = SYSTEM_PAUSED;
 
-    /* 5. Sin eventos pendientes al inicio */
+    // Sin eventos pendientes al inicio
     g_system.pending_event = MANAGER_EVENT_NONE;
 }
